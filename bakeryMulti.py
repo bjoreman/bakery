7cc46a473571f7280ca0ab61cb2ab8ad307cf718
Fredrik Björeman <fredrik@bjoreman.com>
2017-06-03 16:03:42 +0200

    Archive generation, full content of last five posts on index page.

--- bakeryMulti.py
+++ bakeryMulti.py
@@ -16,6 +16,14 @@ import datetime
 import time
 import re
 import multiprocessing
+import traceback
+
+# TODO Remove the need for this
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+def archive_for_tag(tag):
+	return 'archive-' + tag.replace(' ', '-') + '.html'
 
 def should_skip(name):
 	if name.endswith(".txt"):
@@ -29,9 +37,8 @@ def should_skip(name):
 	return True
 
 # Should pass in creation timestamp here I think.
-def create_link_object(path, title, intro, modification_time, tags):
-	path = path.replace('oven/','')
-	#print 'Adding ' + path + ' to links'
+def create_link_object(path, title, intro, modification_time, tags, destination):
+	path = path.replace(destination + '/','')
 	return {'path' : path, 'title': title, 'intro' : intro, 'modified': modification_time, 'tags': tags}
 
 def get_header(bakery, title="Bakery test", scripts=None, root_path=""):
@@ -49,17 +56,71 @@ def get_footer(bakery, root_path=""):
 	result = bakery.footerText.replace("##ROOT_PATH##", root_path)
 	return result
 
+def exc_wrapper(inTuple):
+	try:
+		return generate_page_html(inTuple)
+	except Exception:
+		print "Exception " + inTuple[0]
+		traceback.print_exc()
+		raise
+
+def parse_value(value):
+	if (value == '[]'):
+		return []
+	if (value.startswith('[') and value.endswith(']')):
+		value = value.rstrip(']').lstrip('[').split(',')
+		value = map(unicode.strip, value)
+	return value
+
+def remove_indentation(value):
+	return value.lstrip('-').lstrip(' ')
+
 def generate_page_html(inTuple):
 	(name, path, bakery) = inTuple
 	if should_skip(name):
-		return
+		return None
 	input_file = codecs.open(os.path.join(path, name), mode="r", encoding="utf-8")
 	m_time = os.stat(os.path.join(path, name)).st_mtime
 	modification_time = m_time
 	title = input_file.readline().rstrip("\r\n")
-	lines = []
-	intro = ''
 	tags = []
+	metadata = {}
+	if (title == '---'):
+		nextLine = input_file.readline().rstrip("\r\n")
+		inSubsection = None
+		while (nextLine != title):
+			newSection = nextLine.endswith(':')
+			isIndented = nextLine.startswith((' ', '\t'))
+			data = nextLine.split(':')
+			data = map(unicode.strip, data)
+			if newSection:
+				metadata[data[0]] = []
+				inSubsection = data[0]
+			elif(len(data) > 1):
+				if ((inSubsection is not None) and isIndented):
+					metadata[inSubsection].append(map(remove_indentation, data))
+				else:
+					value = parse_value(data[1])
+					metadata[data[0]] = value
+			elif(len(data) == 1):
+				if inSubsection is not None:
+					metadata[inSubsection].append(remove_indentation(data[0]))
+			nextLine = input_file.readline().rstrip("\r\n")
+		if ('title' in metadata):
+			title = metadata['title']
+		else:
+			print name + ' has no title?'
+			title = 'Nameless'
+	if ('categories' in metadata):
+		if (metadata['categories']):
+			tags.extend(metadata['categories'])
+	if ('tags' in metadata):
+		if (metadata['tags']):
+			tags.extend(metadata['tags'])
+	def strip_quotes(tag):
+		return tag.strip('"')
+	tags = map(strip_quotes, tags)
+	lines = []
 	for line in input_file:
 		if line.startswith('TAGS: '):
 			tags = line.replace('TAGS: ', '', 1).rstrip("\r\n").split(',')
@@ -69,30 +130,31 @@ def generate_page_html(inTuple):
 			modification_time = time.mktime(modification_time.timetuple())
 		else:
 			lines.append(line)
+	if (len(lines) == 0):
+		return None
 	text = ''.join(lines)
 	file = os.path.join(path, name)
 	html = markdown.Markdown().convert(text)
 	root_path = path
-	root_path = root_path.replace('sources/','')
-	#print root_path
+	root_path = root_path.replace(bakery.source + '/','')
 	return_path = ''
 	for element in root_path.split('/'):
-		if(element != 'sources'):
+		if(element != bakery.source):
 			return_path = return_path + '../'
 
-	html = get_header(bakery, title, None, return_path) + html + get_footer(bakery, return_path)
+	full_page = get_header(bakery, title, None, return_path) + html + get_footer(bakery, return_path)
 	# Remove old file extension
 	file = file.replace('.txt', '').replace('.mdown','').replace('.md','').replace('.markdown','')
-	file = file.replace('sources/','')+'.html'
+	file = file.replace(bakery.source + '/','')+'.html'
 	result_path = os.path.join(bakery.destination, file)
 	if m_time > bakery.lastRun:
 		with codecs.open(result_path, mode="w", encoding="utf-8") as dest_file:
-			dest_file.write(html)
+			dest_file.write(full_page)
 	if 'hidden' in tags:
 		print '*** Hidden page, not adding to links'
 		return None
 	else:
-		return create_link_object(result_path,title, html, modification_time, tags)
+		return create_link_object(result_path,title, html, modification_time, tags, bakery.destination)
 
 class Bakery:
 	def __init__(self, source=None, destination=None):
@@ -132,7 +194,7 @@ class Bakery:
 	def build_pages(self, folder):
 		pool = multiprocessing.Pool()
 		for root, dirs, files in os.walk(folder, topdown=False):
-			results = pool.map(generate_page_html, self.zip_with_scalar(files, root))
+			results = pool.map(exc_wrapper, self.zip_with_scalar(files, root))
 			for result in results:
 				if result != None:
 					self.all_links.append(result)
@@ -181,24 +243,57 @@ class Bakery:
 	def sort_links(self):
 		self.all_links.sort(key=self.modified, reverse=True)
 
-	# Use a nice template file for this one too, right.
-	# And skip that template during other generation.
-	# And have a placeholder for content links.
 	def generate_index(self):
-		result = '<ul class="mainList">'
-		#count = 1;
-		for link in self.all_links:
-			result += ('<li><a href="'+link['path']+'">'+link['title']+'</a>')
+		result = ''
+		for index in range(5):
+			link = self.all_links[index]
+			result += '<div class="post">'
 			if link['tags']:
+				result += '<div class="tagWrapper">'
 				for tag in link['tags']:
-					result += '<div class="tags '+ tag +'">' + tag + '</div>'
-			result += ('<p class="datestamp">'+self.format_datetime_for_page(datetime.datetime.fromtimestamp(link['modified']))+'</p>')
-			result += '</li>'
-		result += ('</ul>')
+					result += '<div class="tags '+ tag +'"><a href="'+ archive_for_tag(tag) +'">' + tag + '</a></div>'
+				result += '</div>'
+			result += ('<div class="datestamp">'+self.format_datetime_for_page(datetime.datetime.fromtimestamp(link['modified']))+'</div>')
+			result += '<a href="'+link['path']+'"><h1>' + link['title'] + '</h1></a>'
+			result += link['intro'].replace('../', '')
+			result += '</div>'
 		return_path = ''
 		result = get_header(self, 'bjoreman.com', None, return_path) + result + get_footer(self, return_path)
 		with codecs.open(os.path.join(self.destination, 'index.html'), mode="w", encoding="utf-8") as dest_file:
 			dest_file.write(result)
+
+	def generate_archive_page(self, links, filename, headline, all_tags):
+		result = '<p> Filter by tag: '
+		for tag in all_tags:
+			result += '<a href="' + archive_for_tag(tag) + '">' + tag + '</a> | '
+		result += '<a href="archive.html">All posts</a>'
+		result += '</p>'
+		result += '<ul class="mainList">'
+		for link in links:
+			result +=  ('<li><a href="'+link['path']+'">'+link['title']+'</a>')
+			result += ('<p class="datestamp">'+self.format_datetime_for_page(datetime.datetime.fromtimestamp(link['modified']))+'</p>')
+			result += ('</li>')
+		result += '</ul>'
+		return_path = ''
+		result = get_header(self, headline, None, return_path) + result + get_footer(self, return_path)
+		with codecs.open(os.path.join(self.destination, filename), mode="w", encoding="utf-8") as dest_file:
+			dest_file.write(result)
+
+	def generate_archive(self):
+		all_tags = []
+		for link in self.all_links:
+			all_tags.extend(link['tags'])
+		all_tags = list(set(all_tags))
+		all_tags.sort()
+
+		self.generate_archive_page(self.all_links, 'archive.html', 'Archive - all posts', all_tags)
+
+		for tag in all_tags:
+			tagged_links = []
+			for link in self.all_links:
+				if tag in link['tags']:
+					tagged_links.append(link)
+			self.generate_archive_page(tagged_links, archive_for_tag(tag), 'Archive - posts tagged "' + tag + '"', all_tags)
 
 	def bake(self):
 		print 'Baking ' + self.source + ' to ' + self.destination
@@ -211,6 +306,8 @@ class Bakery:
 		self.generate_index()
 		print 'Creating news and feeds'
 		self.create_news_and_feeds()
+		print 'Creating archive'
+		self.generate_archive()
 		last_run = time.time()
 		with codecs.open(os.path.join(self.destination, '.bakeryData'), mode="w", encoding="utf-8") as dest_file:
 			dest_file.write(str(last_run))
